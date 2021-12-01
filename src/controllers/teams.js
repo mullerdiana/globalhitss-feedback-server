dfc8cebd7dc14dca6a58574cb65da7ed85c02f70
Bruno <brunobergamaschi@live.com>
2021-12-01 14:04:09 -0300

    Criação de teams controller e correção na rota

---
+++ src/controllers/teams.js
@@ -0,0 +1,203 @@
+//chama o Time de dentro de models
+const Teams = require('../models/teams');
+const Users = require("../models/users")
+const status = require('http-status');
+const sequelize = require('../database/sequelize');
+
+//comando para realizar inserção dos dados através de requisição
+exports.Create = (req, res, next) => {
+    //criando variaveis de reconhecimento da requisiçao, de acordo com o que tem no model
+    //lembrando que id é auto incrementavel, nao precisa chama-lo
+    const nome = req.body.nome;
+
+    //Sequelize ira enviar os dados atraves do comando create. create é para inserir
+    Teams.create({
+        nome: nome //nome da chave : constante criada acima
+    }).then(
+        (result) => {
+            if (result) {
+                res.status(status.OK).send(result);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        }
+    ).catch(
+        () => {
+            error = next(error)
+        }
+    )
+
+}
+
+exports.SearchAll = (req, res, next) => {
+    Teams.findAll()
+        .then(
+            (result) => {
+                if (result) {
+                    res.status(status.OK).send(result);
+                }
+            }
+        ).catch(
+            () => {
+                error = next(error)
+            }
+        )
+}
+
+exports.SearchOne = (req, res, next) => {
+    const id = req.params.id;
+
+    Teams.findByPk(id)
+        .then(
+            (result) => {
+                if (result) {
+                    res.status(status.OK).send(result);
+                }
+            }
+        ).catch(
+            () => {
+                error = next(error)
+            }
+        )
+}
+
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+
+    Teams.findByPk(id)
+        .then(
+            (result) => {
+                if (result) {
+                    result.destroy({
+                        where: { id: id }
+                    }).then(
+                        (result) => {
+                            if (result) {
+                                res.status(status.OK).send();
+                            }
+                        }
+                    ).catch(
+                        () => {
+                            error = next(error)
+                        }
+                    )
+                }
+            }
+        ).catch(
+            () => {
+                error = next(error)
+            }
+        )
+}
+
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+
+    Teams.findByPk(id)
+        .then(
+            result => {
+                if (result) {
+                    result.update({
+                            nome: nome,
+                        }, { where: { id: id } })
+                        .then(
+                            (result) => {
+                                if (result) {
+                                    res.status(status.OK).send(result);
+                                }
+                            }
+                        ).catch(
+                            () => {
+                                error => next(error)
+                            }
+                        )
+                }
+            }
+        )
+        .catch(
+            () => {
+                error => next(error)
+            }
+        )
+}
+
+// chave estrangeira - mostra todos os times e seus usuarios
+exports.SearchAllUsersTimes = (req, res, next) => {
+    Teams.findAll({ include: [{ model: Users, as: "users" }] })
+        .then(result => {
+            if (result) {
+                res.status(status.OK).send(result);
+            }
+        }).catch(
+            () => {
+                error = next(error)
+            }
+        )
+}
+
+// chave estrangeira - mostra todos os usuarios de um determinado result
+exports.SearchOneUsersTimes = (req, res, next) => {
+    const id = req.params.id;
+
+    Teams.findByPk(id, { include: [{ model: Users, as: "users" }] })
+        .then(
+            (result) => {
+                if (result) {
+                    res.status(status.OK).send(result);
+                } else {
+                    res.status(status.NOT_FOUND).send();
+                }
+            }
+        ).catch(
+            () => {
+                error = next(error)
+            }
+        )
+}
+
+// chave estrangeira - mostra todos os times e seus formularios
+exports.SearchAllFormsTimes = (req, res, next) => {
+    Teams.findAll({ include: ['forms'] })
+        .then(result => {
+            if (result) {
+                res.status(status.OK).send(result);
+            }
+        }).catch(
+            () => {
+                error = next(error)
+            }
+        )
+}
+
+// chave estrangeira - mostra todos os formularios de um determinado result
+exports.SearchOneFormsTimes = (req, res, next) => {
+    const id = req.params.id;
+
+    Teams.findByPk(id, { include: ['forms'] })
+        .then(
+            (result) => {
+                if (result) {
+                    res.status(status.OK).send(result);
+                } else {
+                    res.status(status.NOT_FOUND).send();
+                }
+            }
+        ).catch(
+            () => {
+                error = next(error)
+            }
+        )
+}
+
+
+
+exports.ContagemTimes = async(req, res, next) => {
+    try {
+        const [response] = await sequelize.query('SELECT count(id) AS count FROM `times`')
+        res.status(status.OK).send(response[0]);
+    } catch (error) {
+        next(error)
+    }
+
+}
