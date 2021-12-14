b56f07ec84c3db58f53b5b53d895ba91567cc2dd
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-14 13:38:20 -0300

    Removido campo username e inserido o  relacionamento com a tabela teams

--- src/models/users.js
+++ src/models/users.js
@@ -1,81 +1,61 @@
 //chamando o sequelize e o arquivo de configuraçao do databese.js
 const Sequelize = require('sequelize');
 const sequelize = require('../database/sequelize');
+const Teams = require('../models/teams');
 
 // montando a estrutura da tabela no Sequelize.
 // sequelize.define define a estrutura que a tabela deve possuir, passando o nome e os campos da tabela
 
 const Users = sequelize.define('users', {
-    name: {
-        allowNull: false,
-        unique: true,
-        type: Sequelize.STRING(255),
-        validate: {
-            len: [3, 255] //define tamanho minimo e maximo do campo
-        }
-    },
-    username: {
-        allowNull: false,
-        unique: true,
-        type: Sequelize.STRING(100),
-        validate: {
-            len: [3, 100]
-        }
-    },
-    email: {
-        allowNull: false,
-        unique: true,
-        type: Sequelize.STRING(100),
-        validate: {
-            len: [3, 100]
-        }
-    },
-    password: {
-        allowNull: false,
-        type: Sequelize.STRING(100),
-        validate: {
-            len: [3, 100]
-        }
-    },
-    type: {
-        allowNull: false,
-        type: Sequelize.STRING(100),
-        validate: {
-            len: [3, 100]
-        }
-    },
-    idTeam: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-        references: {
-            model: 'teams', // refers to table name
-            key: 'id', // 'id' refers to column in table
-        }
-    }
+	name: {
+		allowNull: false,
+		unique: true,
+		type: Sequelize.STRING(255),
+		validate: {
+			len: [3, 255], //define tamanho minimo e maximo do campo
+		},
+	},
+	email: {
+		allowNull: false,
+		unique: true,
+		type: Sequelize.STRING(100),
+		validate: {
+			len: [3, 100],
+		},
+	},
+	password: {
+		allowNull: false,
+		type: Sequelize.STRING(100),
+		validate: {
+			len: [3, 100],
+		},
+	},
+	type: {
+		allowNull: false,
+		type: Sequelize.STRING(100),
+		validate: {
+			len: [3, 100],
+		},
+	},
+	idTeam: {
+		type: Sequelize.INTEGER,
+		allowNull: true,
+		references: {
+			model: 'teams', // refers to table name
+			key: 'id', // 'id' refers to column in table
+		},
+	},
 });
 
-// //chave estrangeira id avaliador dentro da tabela respostas
-// const respostasAvaliador = require('./respostas');
-// Usuarios.hasMany(respostasAvaliador, {
-//     foreignKey: 'idAvaliador', 
-//     onDelete: 'CASCADE', 
-//     onUpdate: 'CASCADE', 
-//     as: 'respsAvaliador'
-// })
-
-// //chave estrangeira id avaliado dentro da tabela respostas
-// const respostasAvaliado = require('./respostas');
-// Usuarios.hasMany(respostasAvaliado, {
-//     foreignKey: 'idAvaliado', 
-//     onDelete: 'CASCADE', 
-//     onUpdate: 'CASCADE', 
-//     as: 'respsAvaliado'
-// })
+Users.belongsTo(Teams, {
+	constraints: true,
+	foreingKey: 'idTeam',
+});
 
 const init = async () => {
-    await Users.sync({alter:true});
-  };
-  
-  init();
+	await Users.sync({ alter: true });
+};
+
+init();
 
 module.exports = Users;
