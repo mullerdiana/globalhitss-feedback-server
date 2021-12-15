8ba93ec30ceccc6eda40ae6c5b29202cce0c8325
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-15 11:40:31 -0300

    correção em DATABASE_PORT no index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.DATABASE_PORT || 5000;
 const app = express();
 
 const usersRoutes = require('./src/routes/users.routes.js');
