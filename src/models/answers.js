25e84e3aee70953038bd0ed10f25b8b4b51c0cea
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-16 11:39:34 -0300

    Answers model: Remoção de linhas de comentários; Ajuste de parâmetros de model;

--- src/models/answers.js
+++ src/models/answers.js
@@ -1,37 +1,24 @@
 //chamando o sequelize e o arquivo de configuraçao do databese.js
-const Sequelize = require('sequelize');
-const sequelize = require('../database/sequelize');
-const Questions = require('./questions');
+const Sequelize = require("sequelize");
+const sequelize = require("../database/sequelize");
+const Questions = require("./questions");
 
 // montando a estrutura da tabela no Sequelize.
 // sequelize.define define a estrutura que a tabela deve possuir, passando o name e os campos da tabela
 
-const Answers = sequelize.define('answers', {
+const Answers = sequelize.define("answers", {
 	value: {
 		allowNull: false,
 		type: Sequelize.STRING(1000),
 		validate: {
-			len: [3, 1000], //define tamanho minimo e maximo do campo
-		},
-	},
-	type: {
-		allowNull: false,
-		type: Sequelize.STRING(255),
-		defaultValue: 'text',
-	},
-	question_id: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		references: {
-			model: Questions,
-			key: 'id',
+			len: [3, 1000],
 		},
 	},
 });
 
 Answers.belongsTo(Questions, {
-	as: 'Questions',
-	foreignKey: 'question_id',
+	as: "Questions",
+	foreignKey: "question_id",
 });
 
 module.exports = Answers;
