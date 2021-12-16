5666aa468649e1bd036343e40d4036ccd9dcd961
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-16 11:40:56 -0300

    Users model: Remoção de linhas de comentários; Ajuste no parâmetros da função;

--- src/models/users.js
+++ src/models/users.js
@@ -1,18 +1,14 @@
-//chamando o sequelize e o arquivo de configuraçao do databese.js
-const Sequelize = require('sequelize');
-const sequelize = require('../database/sequelize');
-const Teams = require('../models/teams');
+const Sequelize = require("sequelize");
+const sequelize = require("../database/sequelize");
+const Teams = require("./teams");
 
-// montando a estrutura da tabela no Sequelize.
-// sequelize.define define a estrutura que a tabela deve possuir, passando o name e os campos da tabela
-
-const Users = sequelize.define('users', {
+const Users = sequelize.define("users", {
 	name: {
 		allowNull: false,
 		unique: true,
 		type: Sequelize.STRING(255),
 		validate: {
-			len: [3, 255], //define tamanho minimo e maximo do campo
+			len: [3, 255],
 		},
 	},
 	email: {
@@ -37,19 +33,11 @@ const Users = sequelize.define('users', {
 			len: [3, 100],
 		},
 	},
-	team_id: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		references: {
-			model: Teams,
-			key: 'id',
-		},
-	},
 });
 
 Users.belongsTo(Teams, {
-	as: 'Teams',
-	foreignKey: 'team_id',
+	as: "Teams",
+	foreignKey: "team_id",
 });
 
 module.exports = Users;
