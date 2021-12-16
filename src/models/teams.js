1b364dc2d188871bfac01d16fc35622c0c0bd964
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-16 10:13:45 -0300

    remoção de linhas não utilizadas

--- src/models/teams.js
+++ src/models/teams.js
@@ -1,7 +1,6 @@
 //chamando o sequelize e o arquivo de configuraçao do database.js
 const Sequelize = require('sequelize');
 const sequelize = require('../database/sequelize');
-const Users = require('./users');
 
 // montando a estrutura da tabela no Sequelize.
 // sequelize.define define a estrutura que a tabela deve possuir, passando o name e os campos da tabela
@@ -15,22 +14,5 @@ const Teams = sequelize.define('teams', {
 		},
 	},
 });
-
-// Teams.belongsToMany(Users, {
-// 	through: 'teamUser',
-// });
-
-// Teams.hasMany(Users, {
-// 	foreignKey: 'idUser',
-// 	onDelete: 'CASCADE',
-// 	onUpdate: 'CASCADE',
-// 	as: 'users',
-// });
-
-// const init = async () => {
-// 	await Teams.sync({ alter: true });
-// };
-
-// init();
 
 module.exports = Teams;
