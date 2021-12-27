73988926ebb588a977a47997d2b668addb447f42
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-27 18:07:29 -0300

    Correção na rota colaboradores - get todos

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -3,13 +3,11 @@ const employeesRoutes = express.Router();
 
 const employeesController = require("../controllers/employees");
 
-
 employeesRoutes.post("/cadastrar", employeesController.Create);
-employeesRoutes.get("/todos/colaboradores", employeesController.SearchAll);
+employeesRoutes.get("/todos", employeesController.SearchAll);
 employeesRoutes.get("/colaborador/:id", employeesController.SearchOne);
 employeesRoutes.get("/search", employeesController.Search);
 employeesRoutes.delete("/deletar/:id", employeesController.Delete);
 employeesRoutes.put("/editar/:id", employeesController.Update);
-
 
 module.exports = employeesRoutes;
