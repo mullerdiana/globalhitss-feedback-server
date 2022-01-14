961efcc982e98920de4bf523b14b1409b589bd97
Bruno Bergamaschi <brunobergamaschi@live.com>
2022-01-14 10:53:45 -0300

    db

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ module.exports = {
 	database: 'd7ilm0j559t6ck',
 	username: 'pzsnndwbzksdzk',
 	password: '5ca4ffb794352871c135b2c55d3c2e108240a2d2df782a44c097652f0fbf2408',
+	port: 5432,
 	host: 'ec2-184-73-243-101.compute-1.amazonaws.com',
 	dialect: 'postgres',
 	define: {
