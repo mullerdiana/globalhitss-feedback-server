2998f9582a4994b09da16e21b298fe4b515bee9b
Bruno Bergamaschi <brunobergamaschi@live.com>
2022-01-27 13:59:46 -0300

    Ajustes de rotas e controllers

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const dbConfig = require("../config/db");
+const dbConfig = require("../config/dbAzure");
 
 const sequelize = new Sequelize(dbConfig);
 
