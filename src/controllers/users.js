93ab764354ad62beddf597c87d2b8508358946f9
Bruno Bergamaschi <brunobergamaschi@live.com>
2022-02-08 10:59:14 -0300

    Ajuste de controllers

---
+++ src/controllers/users.js
@@ -0,0 +1,242 @@
+const Users = require("../models/users");
+const bcrypt = require("bcrypt");
+const status = require("http-status");
+const sequelize = require("../database/sequelize");
+
+exports.Create = (req, res, next) => {
+    const { name, email, password, type } = req.body;
+
+    Users.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, 10),
+        type,
+    })
+        .then((result) => {
+            if (result) {
+                res.status(status.OK).json({ msg: `Usuário criado` });
+            } else {
+                res.status(status.BAD_REQUEST).json({
+                    msg: "Ocorreu um erro imprevisto",
+                });
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(status.BAD_REQUEST).json({
+                msg: "Não foi possível criar o usuário",
+            });
+        });
+};
+
+exports.SearchAll = (req, res, next) => {
+    Users.findAll()
+        .then((result) => {
+            res.status(status.OK).json(result);
+        })
+        .catch(() => {
+            res.status(status.INTERNAL_SERVER_ERROR).json({
+                msg: "Internal Server Error!",
+            });
+        });
+};
+
+exports.Search = async (req, res, next) => {
+    const { search } = req.query;
+
+    const [response] = await sequelize.query(
+        `SELECT * FROM Userss WHERE name LIKE '%${search}%' OR email LIKE '%${search}%'`
+    );
+    //TODO: Validar se ao não encontrar usuário, é correto retornar status 200 e array vazio
+    res.status(status.OK).send(response);
+};
+
+exports.SearchOne = (req, res, next) => {
+    const { id } = req.params;
+
+    Users.findByPk(id)
+        .then((result) => {
+            if (result) {
+                res.status(status.OK).send(result);
+            } else {
+                res.status(status.BAD_REQUEST).json({
+                    msg: "Ocorreu um erro imprevisto",
+                });
+            }
+        })
+        .catch(() => {
+            res.status(status.NOT_FOUND).json({
+                msg: "Colaborador não encontrado",
+            });
+        });
+};
+
+exports.GetByManager = async (req, res, next) => {
+    const { search } = req.query;
+
+    const [response] = await sequelize.query(
+        `SELECT id, name, email, team_id, is_active FROM Userss WHERE manager_id LIKE '%${search}%'`
+    );
+    //TODO: Validar se ao não encontrar usuário, é correto retornar status 200 e array vazio
+    res.status(status.OK).send(response);
+};
+
+exports.GetByManagerAndTeam = async (req, res, next) => {
+    const { manager, team } = req.query;
+
+    const [response] = await sequelize.query(
+        `SELECT id, name, email, team_id, is_active FROM Userss WHERE manager_id LIKE '%${manager}%' AND team_id LIKE '%${team}%'`
+    );
+    //TODO: Validar se ao não encontrar usuário, é correto retornar status 200 e array vazio
+    res.status(status.OK).send(response);
+};
+
+exports.GetByManagerAndTeamNull = async (req, res, next) => {
+    const { manager } = req.query;
+
+    const [response] = await sequelize.query(
+        `SELECT id, name, email, team_id, is_active FROM Userss WHERE manager_id LIKE '%${manager}%' AND team_id IS NULL`
+    );
+    //TODO: Validar se ao não encontrar usuário, é correto retornar status 200 e array vazio
+    res.status(status.OK).send(response);
+};
+
+exports.Delete = (req, res, next) => {
+    const { id } = req.params;
+
+    Users.findByPk(id)
+        .then((result) => {
+            if (result) {
+                result
+                    .destroy({
+                        where: { id: id },
+                    })
+                    .then((result) => {
+                        if (result) {
+                            res.status(status.OK).json({
+                                msg: `Colaborador deletado`,
+                            });
+                        }
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        if (
+                            error.name === "SequelizeForeignKeyConstraintError"
+                        ) {
+                            res.status(status.BAD_REQUEST).json({
+                                msg: "O colaborador possui formulários atribuídos para resposta e não pode ser excluído",
+                            });
+                        } else {
+                            res.status(status.BAD_REQUEST).json({
+                                msg: "Colaborador não encontrado",
+                            });
+                        }
+                    });
+            } else {
+                res.status(status.BAD_REQUEST).json({
+                    msg: "Colaborador não encontrado",
+                });
+            }
+        })
+        .catch(() => {
+            res.status(status.NOT_FOUND).json({
+                msg: "Colaborador não encontrado",
+            });
+        });
+};
+
+exports.Update = (req, res, next) => {
+    const { id } = req.params;
+    const { name, email, password, type, team_id } = req.body;
+
+    Users.findByPk(id)
+        .then((result) => {
+            if (result) {
+                result
+                    .update(
+                        {
+                            name: name,
+                            email: email,
+                            password: bcrypt.hashSync(password, 10),
+                            type: type,
+                            team_id: team_id,
+                        },
+                        { where: { id: id } }
+                    )
+                    .then((result) => {
+                        if (result) {
+                            res.status(status.OK).json({
+                                msg: `Colaborador atualizado`,
+                            });
+                        }
+                    })
+                    .catch((error) => {
+                        if (
+                            error.name === "SequelizeForeignKeyConstraintError"
+                        ) {
+                            res.status(status.NOT_FOUND).json({
+                                msg: "Colaborador não encontrado",
+                            });
+                        }
+                    });
+            } else {
+                res.status(status.BAD_REQUEST).json({
+                    msg: "Ocorreu um erro imprevisto",
+                });
+            }
+        })
+        .catch(() => {
+            res.status(status.NOT_FOUND).json({
+                msg: "Colaborador não encontrado",
+            });
+        });
+};
+
+exports.UpdateTeam = (req, res, next) => {
+    const { id } = req.params;
+    const { team_id } = req.body;
+
+    Users.findByPk(id)
+        .then((result) => {
+            if (result) {
+                result
+                    .update(
+                        {
+                            team_id,
+                        },
+                        { where: { id: id } }
+                    )
+                    .then((result) => {
+                        if (result) {
+                            if (team_id === null) {
+                                res.status(status.OK).json({
+                                    msg: `Colaborador removido do time`,
+                                });
+                            } else {
+                                res.status(status.OK).json({
+                                    msg: `Colaborador adicionado ao time`,
+                                });
+                            }
+                        }
+                    })
+                    .catch((error) => {
+                        if (
+                            error.name === "SequelizeForeignKeyConstraintError"
+                        ) {
+                            res.status(status.NOT_FOUND).json({
+                                msg: "Colaborador não encontrado",
+                            });
+                        }
+                    });
+            } else {
+                res.status(status.BAD_REQUEST).json({
+                    msg: "Ocorreu um erro imprevisto",
+                });
+            }
+        })
+        .catch(() => {
+            res.status(status.NOT_FOUND).json({
+                msg: "Colaborador não encontrado",
+            });
+        });
+};
