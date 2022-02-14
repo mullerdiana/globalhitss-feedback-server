4d7c8f04524cb53eaf3d5d3495ca9f82d3713aa1
Bruno Bergamaschi <brunobergamaschi@live.com>
2022-02-14 17:09:15 -0300

    Implementação de update form -> is_active

--- src/routes/forms.routes.js
+++ src/routes/forms.routes.js
@@ -9,5 +9,6 @@ formsRoutes.get("/form/:id", formsController.SearchOne);
 formsRoutes.get("/get-by-manager", formsController.GetByManager);
 formsRoutes.delete("/delete/:id", formsController.Delete);
 formsRoutes.put("/edit/:id", formsController.Update);
+formsRoutes.patch("/edit-is-active/:id", formsController.UpdateIsActive);
 
 module.exports = formsRoutes;
