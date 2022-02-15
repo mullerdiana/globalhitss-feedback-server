74a2708e83aac43992fc653a5a6ebebd0ca99549
Bruno Bergamaschi <brunobergamaschi@live.com>
2022-02-15 15:14:49 -0300

    Criação de migration-self_evaluation

---
+++ src/database/migrations/11-create-self_evaluation.js
@@ -0,0 +1,58 @@
+"use strict";
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("self_evaluation", {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "users",
+                    key: "id",
+                },
+            },
+            strong: {
+                type: Sequelize.STRING(4000),
+                allowNull: false,
+            },
+            improve: {
+                type: Sequelize.STRING(4000),
+                allowNull: false,
+            },
+            knowledge: {
+                type: Sequelize.STRING(4000),
+                allowNull: false,
+            },
+            skills: {
+                type: Sequelize.STRING(4000),
+                allowNull: false,
+            },
+            attitudes: {
+                type: Sequelize.STRING(4000),
+                allowNull: false,
+            },
+            is_active: {
+                type: Sequelize.INTEGER,
+                defaultValue: 1,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("self_evaluation");
+    },
+};
